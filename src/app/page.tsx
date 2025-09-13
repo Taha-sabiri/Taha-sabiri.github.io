555d22dee86d5a4d27c254ef78948251548fc447
Taha Sabiri <121357512+Taha-sabiri@users.noreply.github.com>
2025-09-13 10:45:29 +0330

    v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,255 @@
 'use client'
+import { Calendar, Info, Star } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import TypewriterComponent from "typewriter-effect";
 
 export default function Home() {
 
-  
+
   return (
-    <div className="min-h-screen w-full px-[4svw] py-[4svh] flex-col">
+    <div className="min-h-screen   py-[4svh] flex-col flex  justify-center items-center   w-full p-10 ">
 
-      <TypewriterComponent
-        options={{
-          delay: 20,
-        }}
-        onInit={(typewriter) => {
-          typewriter.typeString('(* ^_^ *)<br/></TypewriterComponent>Hello!<br/> I’m Taha Sabiri, a detail-oriented Front-End Developer from Semnan, Iran, born in 2000. With over three years of professional experience, I specialize in creating seamless and visually appealing user interfaces using cutting-edge technologies like React and Flutter. My passion for coding stems from a desire to blend creativity with functionality, ensuring that every digital experience I design is engaging and intuitive.<br/>I thrive in collaborative environments, where I can leverage my skills to bring ideas to life. Outside of coding, I actively engage with the tech community, exploring new technologies and continually honing my craft through learning and experimentation. Let’s connect and create something amazing together! <br/><br/>***Skill***<br/>1-javascript<br/>2-typescript<br/>3-react js<br/>4-next js<br/>5-react native<br/>6-sql server<br/>7-dart<br/>8-flutter<br/>9-ui & ux<br/><br/> ***certification***<br/>1-foundations of user exprince (UX) design - Google <br/>2-user experince for web design - Linkedin<br/>3-sql-the complete introduction to sql programming<br/>4-the unlimate sql bootcamp : go from zero to hero<br/>5-the ultimate mysql crash course<br/>6-html css bootstrap - Udemy <br/>7-html css javascript react - Udemy<br/>8-QR code generator using javascript - Udemy<br/>9-java complete course - Udemy<br/>10-javascript fundamental - Udemy<br/>11-learn sql - Udemy<br/><br/>***Experience***<br/>1-evtsp (bank mellat TSP)-frontend developer<br/>2-online Advertiser - frontend developer & uiux designer').start()
-            .callFunction(() => {
-             
-              console.log('String typed out!');
-          
-            })
-           
+      <div className=" max-w-2xl flex flex-col items-start text-left w-full gap-4">
+        <div className="flex gap-2 items-center">
+          <h1 className="text-4xl font-bold">Hey,I'm Taha Sabiri </h1>
+          <div className="px-4 h-fit py-1 rounded-full border border-purple-700 bg-purple-700/10 text-purple-700 flex justify-center items-center">
+            <span className="text-sm">in work</span>
+          </div>
+        </div>
+        <p>
+          Front-End Developer with 4 years of hands-on experience in building responsive, user-friendly, and scalable web applications. Skilled in modern JavaScript frameworks, UI/UX best practices, and performance optimization. Strong problem-solving abilities with a focus on delivering clean, maintainable code and enhancing user experience.
+        </p>
+        <div className="flex gap-2 mt-1">
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <a href="www.linkedin.com/in/tahasabiri">
+              <span>Linkedin</span>
+            </a>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <a href="https://github.com/Taha-sabiri">
+              <span>GitHub</span>
+            </a>
+          </div>
+
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <a href="mailto:taha.sabiri6702@gmail.com">
+              <span>Email</span>
+            </a>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <a href="sms:+989102876702">
+              <span>Message</span>
+            </a>
+          </div>
+
+        </div>
+
+        {/* ------------------ */}
+        <div className="flex  gap-2  text-black/70 mt-5 border-t border-black/10 w-full pt-4 items-center">
+          <Info className="size-5" />
+          <span>Skills</span>
+        </div>
+
+        <div className="flex gap-2 mt-1 flex-wrap">
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Next.js</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>React.js</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Vue.js</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>TypeScript</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>JavaScript</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Figma-Photoshop-Illustrator</span>
+          </div>
+
+
+        </div>
+
+
+        {/* ------------------ */}
+        <div className="flex  gap-2  text-black/70 mt-5 border-t border-black/10 w-full pt-4 items-center">
+          <Info className="size-5" />
+          <span>Familiar with</span>
+        </div>
+        <div className="flex gap-2 mt-1 flex-wrap">
+
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Flutter</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Kotlin</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Sql</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Java</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Dart</span>
+          </div>
+          <div className="py-1 px-4 rounded-full border border-black/10">
+            <span>Node.js</span>
+          </div>
+        </div>
+
+
+
+        {/* ------------------ */}
+
+        <div className="flex  gap-2  text-black/70 mt-5 border-t border-black/10 w-full pt-4 items-center">
+          <Info className="size-5" />
+          <span>Experience</span>
+        </div>
+        <div className="flex gap-2 mt-1 flex-wrap">
+
+          <div className="bg-black/5 p-2 rounded-md" >
+            <h1>EVTSP CO (Member of bank Mellat) - Front-End Developer</h1>
+            <div className="flex gap-2  tems-center  py-2 ">
+              <Calendar className=" text-black/70 size-3" />
+              <span className="text-sm text-black/70 w-full">
+                Oct2023 - Present
+              </span>
+            </div>
+            <p className="text-sm text-black/70">
+              Collaborated in a front-end development team to build and optimize web applications for TSP (Tax Service Provider) platform. Contributed to achieving the official TSP license from the Iranian National Tax Administration through team effort and technical implementation.
+            </p>
+
+          </div>
+
+          <div className="bg-black/5 p-2 rounded-md" >
+            <h1>OnlineAdvertiser - Front-End Developer & UiUx Designer</h1>
+            <div className="flex gap-2  tems-center  py-2 ">
+              <Calendar className=" text-black/70 size-3" />
+              <span className="text-sm text-black/70 w-full">
+                Part time - Apr2019 - Jul2021
+              </span>
+            </div>
+            <p className="text-sm text-black/70">
+              Developed the company’s portfolio website and designed user interfaces for “Melango,” a podcast application. Focused on delivering responsive, user-friendly designs and implementing modern front-end practices for advertising projects.
+            </p>
+
+          </div>
+
+        </div>
+
+
+
+        {/* ------------------ */}
+
+        <div className="flex  gap-2  text-black/70 mt-5 border-t border-black/10 w-full pt-4 items-center">
+          <Star className="size-5" />
+          <span>Licenses & certifications</span>
+        </div>
+
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>Software Architecture and Clean Code Design in OOP</h1>
+          <div className="flex gap-2  tems-center  py-2 ">
+            <Calendar className=" text-black/70 size-3" />
+            <span className="text-sm text-black/70 w-full">
+              Mar2025
+            </span>
+          </div>
+          <p className="text-sm text-black/70">
+            Udemy
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>Next.js</h1>
+          <div className="flex gap-2  tems-center  py-2 ">
+            <Calendar className=" text-black/70 size-3" />
+            <span className="text-sm text-black/70 w-full">
+              Mar2025
+            </span>
+          </div>
+          <p className="text-sm text-black/70">
+            Codeyad
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>Learn SQL in 3 Hours : A tutorial for fast learners</h1>
+          <div className="flex gap-2  tems-center  py-2 ">
+            <Calendar className=" text-black/70 size-3" />
+            <span className="text-sm text-black/70 w-full">
+              Apr2024
+            </span>
+          </div>
+          <p className="text-sm text-black/70">
+            Udemy
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>Html css bootstrap</h1>
+          <div className="flex gap-2  tems-center  py-2 ">
+            <Calendar className=" text-black/70 size-3" />
+            <span className="text-sm text-black/70 w-full">
+              Aug2022
+            </span>
+          </div>
+          <p className="text-sm text-black/70">
+            Udemy
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>Foundations of User Experience (UX) Design</h1>
+          <div className="flex gap-2  tems-center  py-2 ">
+            <Calendar className=" text-black/70 size-3" />
+            <span className="text-sm text-black/70 w-full">
+              Aug2022
+            </span>
+          </div>
+          <p className="text-sm text-black/70">
+            Google
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>User Experience for Web Design</h1>
+          <p className="text-sm text-black/70">
+            LinkedIn
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>The Ultimate SQL Bootcamp : Go From Zero to Hero</h1>
+          <p className="text-sm text-black/70">
+            Udemy
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>SQL- The Complete Introduction to SQL programming</h1>
+          <p className="text-sm text-black/70">
+            Udemy
+          </p>
+
+        </div>
+        <div className="bg-black/5 p-2 rounded-md w-full" >
+          <h1>JavaScript Fundamentals</h1>
+          <p className="text-sm text-black/70">
+            Udemy
+          </p>
+
+        </div>
+
+      </div>
 
 
 
 
-        }}
-
-      />
 
     </div>
   );
